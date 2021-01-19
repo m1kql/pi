d85f0e7881a9d33e3edf3fcb83ec6c8fc3318af4
yak-fumblepack <mike.liang2006@gmail.com>
2021-01-19 12:22:34 -0500

    Fix up commands

--- bot/commands/math/contestproblems.py
+++ bot/commands/math/contestproblems.py
@@ -14,19 +14,45 @@ class Contestproblems(commands.Cog):
         print('Contest Math cog has been loaded sucessfully')
 
     @commands.command()
-    async def fetch(self, ctx, contest_type=None, year=None, contest_id=None, problem_num=None):
-        if contest_type != None:
-            requested_contest = str(contest_type.upper())
-        if year != None:
+    async def fetchamc(self, ctx, year=None, contest_id=None, problem_num=None):
+        if year != None and problem_num != None and contest_id != None:
             requested_year = str(year)
-        if contest_id != None:
             requested_id = str(contest_id.upper())
-        if problem_num != None:
             requested_problem = str(problem_num)
-        try:
-            await ctx.send(f'https://raw.githubusercontent.com/yak-fumblepack/pi/master/bot/{requested_contest}/{requested_year}/{requested_id}/{requested_problem}/statement.png')
-        except:
-            await ctx.send("Sorry there was an error processing this command")
+            try:
+                await ctx.send(f'https://raw.githubusercontent.com/yak-fumblepack/mathcontests/master/AMC/{requested_year}/{requested_id}/{requested_problem}/statement.png')
+            except:
+                await ctx.send("Sorry there was an error processing this command")
+    
+    @commands.command()
+    async def fetchaime(self, ctx, year=None, problem_num=None):
+        if year!=None and problem_num !=None:
+            requested_year = str(year)
+            requested_problem = str(problem_num)
+            try:
+                await ctx.send(f'https://raw.githubusercontent.com/yak-fumblepack/mathcontests/master/AIME/{requested_year}/{requested_problem}/statement.png')
+            except:
+                await ctx.send("Sorry there was an error processing this command")
+
+    @commands.command()
+    async def fetchusamo(self, ctx, year=None, problem_num=None):
+        if year!=None and problem_num!=None:
+            requested_year = str(year)
+            requested_problem = str(problem_num)
+            try:
+                await ctx.send(f'https://raw.githubusercontent.com/yak-fumblepack/mathcontests/master/USAMO/{requested_year}/{requested_problem}/statement.png')
+            except:
+                await ctx.send("Sorry there was an error processing this command")
+    
+    @commands.command()
+    async def fetchusajmo(self, ctx, year=None, problem_num=None):
+        if year!=None and problem_num!=None:
+            requested_year = str(year)
+            requested_problem = str(problem_num)
+            try:
+                await ctx.send(f'https://raw.githubusercontent.com/yak-fumblepack/mathcontests/master/USAJMO/{requested_year}/{requested_problem}/statement.png')
+            except:
+                await ctx.send("Sorry there was an error processing this command")
 
     @commands.command()
     async def last5(self, ctx):
@@ -35,7 +61,7 @@ class Contestproblems(commands.Cog):
         randomyear = str(int(random.randint(2002, 2019)))
         randomcontestid = str(random.choice(contestid))
         try:
-            await ctx.send(f'https://raw.githubusercontent.com/yak-fumblepack/pi/master/bot/AMC/{randomyear}/{randomcontestid}/{lastfive}/statement.png')
+            await ctx.send(f'https://raw.githubusercontent.com/yak-fumblepack/mathcontests/master/AMC/{randomyear}/{randomcontestid}/{lastfive}/statement.png')
         except:
             await ctx.send("Sorry there was an error processing this command")
 
@@ -47,14 +73,43 @@ class Contestproblems(commands.Cog):
 
         if difficulty == "easy" or difficulty == "e":
             easy= str(int(random.randint(1, 10)))
-            await ctx.send(f'https://raw.githubusercontent.com/yak-fumblepack/pi/master/bot/AMC/{randomyear}/{randomcontestid}/{easy}/statement.png')
+            await ctx.send(f'https://raw.githubusercontent.com/yak-fumblepack/mathcontests/master/AMC/{randomyear}/{randomcontestid}/{easy}/statement.png')
         elif difficulty == "med" or difficulty == "medium" or difficulty == "m":
             med = str(int(random.randint(11,16)))
-            await ctx.send(f'https://raw.githubusercontent.com/yak-fumblepack/pi/master/bot/AMC/{randomyear}/{randomcontestid}/{med}/statement.png')
+            await ctx.send(f'https://raw.githubusercontent.com/yak-fumblepack/mathcontests/master/AMC/{randomyear}/{randomcontestid}/{med}/statement.png')
         elif difficulty == "hard" or difficulty == "h":
             hard = str(int(random.randint(17, 25)))
-            await ctx.send(f'https://raw.githubusercontent.com/yak-fumblepack/pi/master/bot/AMC/{randomyear}/{randomcontestid}/{hard}/statement.png')
-            
+            await ctx.send(f'https://raw.githubusercontent.com/yak-fumblepack/mathcontests/master/AMC/{randomyear}/{randomcontestid}/{hard}/statement.png')
+
+    @commands.command()
+    async def amc12(self, ctx, difficulty):
+        amc12_id = ["12A", "12B"]
+        randomyear = str(int(random.randint(2002, 2019)))
+        randomcontestid = str(random.choice(amc12_id))
+
+        if difficulty == "easy" or difficulty == "e":
+            easy= str(int(random.randint(1, 10)))
+            await ctx.send(f'https://raw.githubusercontent.com/yak-fumblepack/mathcontests/master/AMC/{randomyear}/{randomcontestid}/{easy}/statement.png')
+        elif difficulty == "med" or difficulty == "medium" or difficulty == "m":
+            med = str(int(random.randint(11,16)))
+            await ctx.send(f'https://raw.githubusercontent.com/yak-fumblepack/mathcontests/master/AMC/{randomyear}/{randomcontestid}/{med}/statement.png')
+        elif difficulty == "hard" or difficulty == "h":
+            hard = str(int(random.randint(17, 25)))
+            await ctx.send(f'https://raw.githubusercontent.com/yak-fumblepack/mathcontests/master/AMC/{randomyear}/{randomcontestid}/{hard}/statement.png')
+    
+    @commands.command()
+    async def random(self, ctx, contest_type=None):
+        if contest_type == 'amc' or contest_type == 'AMC':
+            amc_id = ["10A", "12A", "12A", "12B"]
+            randomcontestid = str(random.choice(amc_id))
+            randomyear = str(int(random.randint(2002, 2019)))
+            problem_num = str(int(random.randint(1, 25)))
+            await ctx.send(f'https://raw.githubusercontent.com/yak-fumblepack/mathcontests/master/AMC/{randomyear}/{randomcontestid}/{problem_num}/statement.png')
+        if contest_type == 'aime' or contest_type == 'AIME':
+            randomyear = str(int(random.randint(2000, 2019)))
+            contest_id = str(int(random.randint(1,2)))
+            problem_num = str(int(random.randint(1, 15)))
+            await ctx.send(f'https://raw.githubusercontent.com/yak-fumblepack/mathcontests/master/AIME/{randomyear}/{contest_id}/{problem_num}/statement.png')
 
 def setup(bot):
     bot.add_cog(Contestproblems(bot))
