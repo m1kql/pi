261c0e4e65fa410ff26e78248f8fa6c653484714
yak-fumblepack <mike.liang2006@gmail.com>
2021-01-21 19:50:11 -0500

    New update for utils, create requirements

--- bot/utils/help.py
+++ bot/utils/help.py
@@ -1,4 +1,5 @@
 import discord
+from bot.utils.commands import *
 from discord.ext import commands
 
 
@@ -14,89 +15,33 @@ class Help(commands.Cog):
     @commands.command()
     async def help(self, ctx, arg=None):
         if arg is None:
-            await ctx.send("""
-            ```
-            \n
-
-            help commands
-            -------
-            Categories: math, code, music
-            Prefix: ^
-
-            help <category> (e.g help math)
-
-            ```
-            """)
+            await ctx.send(default_help)
 
         elif arg == 'code':
-            await ctx.send("""
-            ```
-            \n
-
-            Code:
-            -------
-            Supported Languages: ada, assembly, bash, c#, c++, c, clojure, common lisp, d, elixir, erlang, f#, fortran, go, haskell, java, javascript, kotlin, lua, node.js, ocaml, octave, obj-c, pascal, perl, php, prolog, python, python3, R, rust, ruby, scala, scheme, swift, tcl, visual basic
-
-            run <language> <your program> (e.g run rust fn main() {let sum = 5+10; println!("{}", sum);})
-
-            ```
-            """)
-            await ctx.send("would you like to go back?")
+            await ctx.send(code_help)
+            await ctx.send(goback)
             def check(m):
                 return m.author.id == ctx.author.id
             returnmsg = await self.bot.wait_for('message',check=check)
             if returnmsg.content == 'yes' or returnmsg.content=='ye' or returnmsg.content=='yee':
-                await ctx.send("""
-                ```
-                \n
-                help commands
-                -------
-                Categories: math, code, music
-                Prefix: ^
-
-                help <category> (e.g help math)
-
-                ```
-                """)
+                await ctx.send(default_help)
             else:
                 await ctx.send("ok")
 
         elif arg == 'math':
-            await ctx.send("""
-            ```
-            \n
-
-            Math:
-            -------
-            Supported contests: AMC, AIME, USAMO, USAJMO
-
-            fetch<contest type> <year> <id or no id> <problem number> (e.g fetchamc 2013 10a 12) - fetches a specific problem
-            random <contest type> (e.g random usamo) - returns any problem from a specified contest
-            last5 - returns the last 5 question of any AMC contest
-            amc10 <difficulty> (e.g amc10 med) - returns an amc10 question of that difficulty 
-            amc12 <difficulty> (e.g amc12 e) - returns an amc12 question of that difficulty
-
-            ```
-            """)
-            await ctx.send("would you like to go back?")
+            await ctx.send(math_help)
+            await ctx.send(goback)
             def check(m):
                 return m.author.id == ctx.author.id
             returnmsg = await self.bot.wait_for('message',check=check)
             if returnmsg.content == 'yes' or returnmsg.content=='ye' or returnmsg.content=='yee':
-                await ctx.send("""
-                ```
-                \n
-                help commands
-                -------
-                Categories: math, code, music
-                Prefix: ^
-
-                help <category> (e.g help math)
-
-                ```
-                """)
+                await ctx.send(default_help)
             else:
                 await ctx.send("ok")
     
+    @commands.command()
+    async def invite(self, ctx):
+        await ctx.send(invite)
+
 def setup(bot):
     bot.add_cog(Help(bot))
--- readme.md
+++ readme.md
@@ -1,9 +1,8 @@
-### Pi Discord Bot
+# Pi Discord Bot
 ![GitHub](https://img.shields.io/github/license/yak-fumblepack/pi?style=plastic)
 
 
-An academics focused discord bot that is aimed at helping students with an interest in mathematics to succeed. Can pull problems from the AOPS wiki, run code, and much more. 
-
+An academics focused discord bot that is aimed at helping students with an interest in STEM to succeed. Can pull problems from the AOPS wiki, run code, and much more. 
 
 Supported Contests include: 
 - AMC 10 & 12
@@ -11,3 +10,23 @@ Supported Contests include:
 - USAMO
 - USAJMO
 
+Here is a [link](https://github.com/yak-fumblepack/mathcontests) to the repo containing all of the latex files, png files, and text files to the contest problems. 
+
+
+**Update - 2021-01-21: New problem points system introduced. Improved help command documentation**
+
+### Build
+Clone the repo, `https://github.com/yak-fumblepack/pi.git`, and if you would like to use my other repo containing all the solutions for the contest, don't change anything within `contestproblems.py`. It would help to create a `.env` file to hold all of your secrets. The only secret needed here is the bot token. 
+
+Run `pip install -r requirements.txt` to install all of the packages needed for this project. Python 3.9 is recommended but python 3.8 is best.
+
+That's all you need to host the bot locally, but if you do want to host it 24/7, here's a few links that might help.
+
+- [Extremely detailed, how to host on heroku](https://www.writebots.com/discord-bot-hosting/)
+- [Very detailed, how to host on repl.it](https://repl.it/talk/learn/Hosting-discordpy-bots-with-replit/11008)
+
+### Contributing
+Contributions are most welcome and appreciated. Please make a seperate branch and then we will merge it to `dev` before pushing to `master` after approval. It would also be greatly appreciated if you run autopep8 to format before pushing. 
+
+## License 
+GPL-3.0 - see [LICENSE](https://github.com/yak-fumblepack/pi/blob/master/LICENSE) file for mode details.
