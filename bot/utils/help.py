2eff6d8f50ba79e03415696385bb7c4f7639807c
yak-fumblepack <mike.liang2006@gmail.com>
2021-01-21 22:41:46 -0500

    New help commands for math and help

--- bot/utils/help.py
+++ bot/utils/help.py
@@ -1,5 +1,5 @@
 import discord
-from bot.utils.commands import *
+from bot.utils.sample_embeds import *
 from discord.ext import commands
 
 
@@ -15,33 +15,34 @@ class Help(commands.Cog):
     @commands.command()
     async def help(self, ctx, arg=None):
         if arg is None:
-            await ctx.send(default_help)
+            help_embed = discord.Embed(title="Help", description="All the relevant info", color=0xD6C18A)
+            help_embed.add_field(name="```^```", value="The Prefix", inline=False)
+            help_embed.add_field(name="```^help <command category>```", value="for more info on a specific category of commands and all the commands within it")
+            await ctx.send(embed=help_embed)
+            help_embed2 = discord.Embed(title="Help Pt.2", description="Avalible Categories", color=0xD6C18A)
+            help_embed2.add_field(name="```math```", value="math category")
+            help_embed2.add_field(name="```code```", value="code category")
+            help_embed2.add_field(name="```music```", value="music category")
+            await ctx.send(embed=help_embed2)
+        elif arg == 'math' or arg == 'Math':
+            math_embed = discord.Embed(title="Math Commands", description="All the relevant info", color=0xD6C18A)
+            math_embed.add_field(name="```^fetchamc <year> <contest version> <problem number>```", value="fetches any AIME problem from any given year")
+            math_embed.add_field(name="```^fetchaime <year> <contest version> <problem number>```", value="fetches any AIME problem from any given year")
+            math_embed.add_field(name="```^fetchusamo <year> <problem number>```", value="fetches any USAMO problem from any given year")
+            math_embed.add_field(name="```^fetchusajmo <year> <problem number>```", value="fetches any USAJMO problem from any given year")
+            await ctx.send(embed=math_embed)
+            math_embed2 = discord.Embed(title="Math Commands Pt.2", description="Ranked Commands - Either gain or lose points", color=0xD6C18A)
+            math_embed.add_field(name="```^stats```", value="gives you your statistics. Type `^help statistics` for more info")
+            math_embed.add_field(name="```^last5 <year>```", value="returns the last 5 of any AMC contest (Ranked)")
+            math_embed.add_field(name="```^amc10 <diffculty level>```", value="gives any AMC10 problem of that difficulty (Ranked)")
+            math_embed.add_field(name="```^amc12 <diffculty level>```", value="gives any AMC12 problem of that difficulty (Ranked)")
+            math_embed.add_field(name="```^random <contest name>```", value="fetches any problem from a specified contest (Ranked)")
+            await ctx.send(embed=math_embed2)
 
-        elif arg == 'code':
-            await ctx.send(code_help)
-            await ctx.send(goback)
-            def check(m):
-                return m.author.id == ctx.author.id
-            returnmsg = await self.bot.wait_for('message',check=check)
-            if returnmsg.content == 'yes' or returnmsg.content=='ye' or returnmsg.content=='yee':
-                await ctx.send(default_help)
-            else:
-                await ctx.send("ok")
 
-        elif arg == 'math':
-            await ctx.send(math_help)
-            await ctx.send(goback)
-            def check(m):
-                return m.author.id == ctx.author.id
-            returnmsg = await self.bot.wait_for('message',check=check)
-            if returnmsg.content == 'yes' or returnmsg.content=='ye' or returnmsg.content=='yee':
-                await ctx.send(default_help)
-            else:
-                await ctx.send("ok")
-    
     @commands.command()
     async def invite(self, ctx):
-        await ctx.send(invite)
+        await ctx.send("<https://discord.com/api/oauth2/authorize?client_id=799423483968618566&permissions=0&scope=bot>")
 
 def setup(bot):
     bot.add_cog(Help(bot))
