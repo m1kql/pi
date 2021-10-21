8b75a15b4d401958da521ccecb3653944dbf9812
yntl <92696731+yntl@users.noreply.github.com>
2021-10-20 23:54:39 -0400

    core: rewrite problem display to use embeds

--- bot/src/cogs/utility/db.py
+++ bot/src/cogs/utility/db.py
@@ -21,20 +21,25 @@ firebase_client_x509_cert_url = os.getenv("firebase_client_x509_cert_url")
 
 def buildJSON():
     with open("firebase_config.json", "w", encoding="utf-8") as f:
-        f.write(json.dumps({
-            "type": firebase_type,
-            "project_id": firebase_project_id,
-            "private_key_id": firebase_private_key_id,
-            "private_key": firebase_private_key,
-            "client_email": firebase_client_email,
-            "client_id": firebase_client_id,
-            "auth_uri": firebase_auth_uri,
-            "token_uri": firebase_token_uri,
-            "auth_provider_x509_cert_url": firebase_auth_provider_x509_cert_url,
-            "client_x509_cert_url": firebase_client_x509_cert_url
-        }))
+        f.write(
+            json.dumps(
+                {
+                    "type": firebase_type,
+                    "project_id": firebase_project_id,
+                    "private_key_id": firebase_private_key_id,
+                    "private_key": firebase_private_key,
+                    "client_email": firebase_client_email,
+                    "client_id": firebase_client_id,
+                    "auth_uri": firebase_auth_uri,
+                    "token_uri": firebase_token_uri,
+                    "auth_provider_x509_cert_url": firebase_auth_provider_x509_cert_url,
+                    "client_x509_cert_url": firebase_client_x509_cert_url,
+                }
+            )
+        )
 
     return "firebase_config.json"
+
 
 appCredentials = credentials.Certificate(buildJSON())
 firebase_admin.initialize_app(appCredentials)
